32ea1205f3473bd5d6d65b0c0f6d980a6debeda3
harishbabuv <harish.shilpa@gmail.com>
2020-12-10 18:38:45 +0100

    Commit #4 Email and Password auth using firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCEYBEcsOn833uZ2yZXxH1zHirY0_fkwJg",
+    authDomain: "react-web-ecommerce.firebaseapp.com",
+    projectId: "react-web-ecommerce",
+    storageBucket: "react-web-ecommerce.appspot.com",
+    messagingSenderId: "503541173373",
+    appId: "1:503541173373:web:53d7b38ca87a2328cfb514",
+    measurementId: "G-95ZQ62Q20X"
+  };
